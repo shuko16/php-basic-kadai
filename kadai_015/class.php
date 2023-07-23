45b556fc466ecfe5ecf2917528e3814a4d306b9b
shuko16 <130825656+shuko16@users.noreply.github.com>
2023-07-23 17:48:46 +0900

    Update class.php

--- kadai_015/class.php
+++ kadai_015/class.php
@@ -7,52 +7,48 @@
 
   <body>
     <p>
-      <?php
-      class Food {
-        public $name;
-        public $price;
+    <?php
+class Food
+{
+  private $name;
+  private $price;
 
-        public function set_name(string $name) {
-        $this->name=$name;
-        } 
-        public function show_name() {
-        echo $this->name . '<br>';
-      }
-
-      public function set_price(int $price) {
-        $this->price=$price;
-        } 
-        public function show_price() {
-        echo $this->price . '<br>';
-      }
-
-    public function _construct(string $name, int $price) {
-        $this->name = $name;
-        $this->price = $price;
-        
-print_r($food)
-    ?>
-
-<?php
-Class Animal {
-    public $name;
-    public $height;
-    public $weight;
-
-    public function set_height(int $height) {
-    $this->height=$height;
-    }
-    public function show_height(){
-    echo $this ->height . '<br>'
-    }
-
-  public function _construct(string $name, int $height, int $weight) {
+  public function __construct(string $name, int $price)
+  {
     $this->name = $name;
+    $this->price = $price;
+  }
+
+  public function show_name()
+  {
+    echo $this->name . '<br>';
+  }
+}
+
+class Animal
+{
+  private $name;
+  private $height;
+  private $weight;
+
+  public function __construct(string $name, int $height, int $weight)
+  {
+    $this->name - $name;
     $this->height = $height;
     $this->weight = $weight;
+  }
+
+  public function show_name()
+  {
+    echo $this->height . '<br>';
+  }
 }
-}
-$user = new Animal('dog', 60, 5000);
+
+$food = new Food('poteto', 250);
+
+$animal = new Animal('dog', 60, 5000);
+
+print_r($food);
 print_r($animal);
 
 ?>
